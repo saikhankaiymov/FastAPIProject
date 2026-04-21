925f81d510df0aedae9b935f0b2f5fd9cdcf57a4
Saikhan <saikhankaiymov@gmail.com>
2026-04-22 00:24:34 +0600

    feat: add hello endpoint

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/hello")
+async def say_hello_to_world():
+    return {"message": "Hello World"}
